55281a783a98e48b820459ab20772fd88dd3c93c
khj1637 <37471933+khj1637@users.noreply.github.com>
2025-06-17 09:46:36 +0900

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -176,7 +176,7 @@ if st.button("📊 예측 실행"):
         total_non_work_days = non_work1 + non_work2
         work3 = total_days - total_non_work_days
 
-        st.markdown("## 가동률 분석")
+        st.subheader("📌 가동률 분석")
         col1, col2, col3 = st.columns(3)
 
         with col1:
